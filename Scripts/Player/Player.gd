a3dc72a0d5730250a0d7ce82e7428179d8d3be40
Robert Peters <robip2305@gmail.com>
2026-07-23 17:44:10 +0200

    Double Jump + Jump Buffer

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+var curJumps = 2
 
 const SPEED:float = 5.0
 const JUMP_VELOCITY:float = 4.5
@@ -28,27 +29,47 @@ var vel : Vector3 = Vector3()
 var mouseDelta : Vector2 = Vector2()
 
 #Normal Weapon
+@export_category("Weapon")
 @export var bullet: PackedScene
 @export var muzzle : Node3D 
 @export var bulletsPerBurst : int = 3
 @export var burstSpeed : float = 0.05
 @export var burstCooldown : float = 0.5
 @export var tracerSpeed : float = 0.1
-var isShooting : bool = false
 var curShootTimer : float = 0
 var curBurstCountdown : int = bulletsPerBurst - 1
 
+#inputs
+@export_category("Inputs")
+var isShooting : bool = false
+@export var jumpBuffer : float = 0.05
+var curJumpBuffered : float = jumpBuffer
+
 # player components
-@onready var camera: Camera3D= get_node("Camera3D")
+@onready var camera: Camera3D = get_node("Camera3D")
+
+func _process(delta):
+	controll_camera(delta)
+	if(isShooting):
+		shoot(delta)
+	
+	curJumpBuffered -= delta
+	if(Input.is_action_just_pressed("jump")):
+		curJumpBuffered = jumpBuffer
+	
+	if(is_on_floor()):
+		curJumps = 2
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if curJumpBuffered >= 0 and curJumps > 0:
+		curJumpBuffered = -1
 		velocity.y = JUMP_VELOCITY
+		curJumps -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -78,17 +99,12 @@ func controll_camera(delta:float):
 	camera.rotation_degrees -= Vector3(rad_to_deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
 	# clamp the vertical camera rotation
 	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
-
+	
 	# rotate player along Y axis
 	rotation_degrees -= Vector3(0, rad_to_deg(mouseDelta.x), 0) * lookSensitivity * delta
-
+	
 	# reset the mouse delta vector
 	mouseDelta = Vector2()
-
-func _process(delta):
-	controll_camera(delta)
-	if(isShooting):
-		shoot(delta)
 
 func shoot(delta):
 	curShootTimer -= delta
