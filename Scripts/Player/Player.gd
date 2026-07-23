b989b0e2017e6497612cd534e653641f47ef2810
Robert Peters <robip2305@gmail.com>
2026-07-23 21:45:45 +0200

    Start of dash

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,22 +1,37 @@
 extends CharacterBody3D
 
-
-const SPEED:float = 5.0
-const JUMP_VELOCITY:float = 4.5
-const SHOOT_SHAKE_AMOUNT:float = 0.01
+const SHOOT_SHAKE_AMOUNT:float = 0.05
 signal camera_shake(amount:float)
-
 
 # stats
 var curHp : int = 10
 var maxHp : int = 10
-
 var score : int = 0
 
 # physics
-var moveSpeed : float = 5.0
-var jumpForce : float = 5.0
 var gravity : float = 12.0
+
+# movement
+@export_subgroup("Movement")
+@export var moveSpeed : float = 5.0
+@export var sprintSpeed : float = 12.0
+var curSpeed : float = moveSpeed
+
+# jump
+@export_subgroup("Jump")
+@export var jumpForce : float = 5.0
+@export var curJumps = 2
+@export var jumpBuffer : float = 0.05
+var curJumpBuffered : float = jumpBuffer
+
+# dash
+@export_subgroup("Dash")
+@export var dashBuffer : float = 0.05
+var curDashBuffered : float = dashBuffer
+@export var dashDuration : float = 0.2
+@export var dashCooldown : float = 0.5
+var curDashTimer : float = 0
+var isDashing : bool = true
 
 # cam look
 var minLookAngle : float = -90.0
@@ -28,38 +43,68 @@ var vel : Vector3 = Vector3()
 var mouseDelta : Vector2 = Vector2()
 
 #Normal Weapon
+@export_category("Weapon")
 @export var bullet: PackedScene
 @export var muzzle : Node3D 
 @export var bulletsPerBurst : int = 3
 @export var burstSpeed : float = 0.05
 @export var burstCooldown : float = 0.5
 @export var tracerSpeed : float = 0.1
-var isShooting : bool = false
 var curShootTimer : float = 0
 var curBurstCountdown : int = bulletsPerBurst - 1
+var isShooting : bool = false
+
 
 # player components
-@onready var camera: Camera3D= get_node("Camera3D")
+@onready var camera: Camera3D = get_node("Camera3D")
+
+func _process(delta):
+	controll_camera(delta)
+	if(isShooting):
+		shoot(delta)
+	
+	curJumpBuffered -= delta
+	if(Input.is_action_just_pressed("jump")):
+		curJumpBuffered = jumpBuffer
+	
+	if(is_on_floor()):
+		curJumps = 2
+	
+	curDashBuffered -= delta
+	if(Input.is_action_just_pressed("dash")):
+		curDashBuffered = dashBuffer
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
+	# Handle Jump
+	if curJumpBuffered >= 0 and curJumps > 0:
+		curJumpBuffered = -1
+		velocity.y = jumpForce
+		curJumps -= 1
+	
+	# Handle Dashing
+	curDashTimer -= delta
+	if(curDashBuffered >= 0 and curDashTimer <= 0 and !isDashing):
+		curDashTimer = dashDuration
+		#Dash here
+	
+	# Handle Sprinting
+	curSpeed = moveSpeed
+	if(Input.is_action_pressed("sprint")):
+		curSpeed = sprintSpeed
+	
+	# Handle Movement
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * curSpeed
+		velocity.z = direction.z * curSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, curSpeed)
+		velocity.z = move_toward(velocity.z, 0, curSpeed)
 	
 	move_and_slide()
 	
@@ -78,17 +123,12 @@ func controll_camera(delta:float):
 	camera.rotation_degrees -= Vector3(rad_to_deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
 	# clamp the vertical camera rotation
 	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
-
+	
 	# rotate player along Y axis
 	rotation_degrees -= Vector3(0, rad_to_deg(mouseDelta.x), 0) * lookSensitivity * delta
-
+	
 	# reset the mouse delta vector
 	mouseDelta = Vector2()
-
-func _process(delta):
-	controll_camera(delta)
-	if(isShooting):
-		shoot(delta)
 
 func shoot(delta):
 	curShootTimer -= delta
@@ -103,10 +143,6 @@ func shoot(delta):
 		else:
 			curBurstCountdown = bulletsPerBurst - 1
 			curShootTimer = burstCooldown
-	
-	camera_shake.emit(SHOOT_SHAKE_AMOUNT)
-	await SlowMotion.slow_motion(5)
-	
 
 func shootProjectile():
 	var space_state = get_world_3d().direct_space_state
@@ -119,17 +155,18 @@ func shootProjectile():
 
 	if result:
 		# Damage the target
-		# if result.collider.has_method("take_damage"):
-		# 	result.collider.take_damage(20)
 		
 		spawn_tracer(from, result.position)
 	else:
 		spawn_tracer(from, to)
+	
+	camera_shake.emit(SHOOT_SHAKE_AMOUNT)
+	await SlowMotion.slow_motion(5)
 
 func spawn_tracer(from: Vector3, to: Vector3):
 	if bullet == null:
 		return
-	print("Bullet")
+	
 	var tracer = bullet.instantiate()
 	get_tree().current_scene.add_child(tracer)
 	tracer.initialize(from, to, tracerSpeed)
