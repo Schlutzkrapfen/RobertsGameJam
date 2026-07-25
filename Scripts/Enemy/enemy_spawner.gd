e1f05fe39cc7ae717c3375bb6f75c1a1a9460bd3
Schlutzkrapfen <david.messner@protonmail.com>
2026-07-25 09:55:58 +0200

    added a sphere

--- Scripts/Enemy/enemy_spawner.gd
+++ Scripts/Enemy/enemy_spawner.gd
@@ -8,13 +8,11 @@ extends Node
 func _on_boss_spawn_enemies() -> void:
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
-
 	# Choose a random location on the SpawnPath.
 	# We store the reference to the SpawnLocation node.
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
 	# And give it a random offset.
 	mob_spawn_location.progress_ratio = randf()
-
 	#var player_position =player.position
 	#mob.initialize(mob_spawn_location.position, player_position)
 	mob.target = player
--- Scripts/Enemy/spehere_attack.gd
+++ Scripts/Enemy/spehere_attack.gd
@@ -5,14 +5,19 @@ var time_tile_attack:float = 4.0
 var time_for_attack:float = 0.2
 var time_for_reset:float = 0.2
 
+var enemies:Array[Node] 
+
 var attack_height:float = 200
 
 func _ready() -> void:
 	
 	var unique_mesh = $MeshInstance3D.mesh.duplicate()
 	$MeshInstance3D.mesh = unique_mesh
+	var unique_collision = $MeshInstance3D/Area3D/CollisionShape3D.shape.duplicate()
 	var cur_height = $MeshInstance3D.mesh.height
+	var tween2 = get_tree().create_tween()
 	var tween = get_tree().create_tween()
+	tween2.tween_property(unique_collision, "radius", attack_size, time_tile_attack).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(unique_mesh, "radius", attack_size, time_tile_attack).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(unique_mesh, "height", attack_height, time_for_attack).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(unique_mesh, "height", cur_height, time_for_reset).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
@@ -23,3 +28,7 @@ func delete():
 	visible = false
 	queue_free()
 	
+
+
+func _on_collision_shape_3d_child_entered_tree(node: Node) -> void:
+	enemies.append(node)
