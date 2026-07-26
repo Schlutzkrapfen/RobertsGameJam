eb0a405e651cc367b275d597c8c465276896596b
David <fhs49586>
2026-07-26 17:03:55 +0200

    fixed merge conflikt

--- Scripts/Enemy/GameManager.gd
+++ Scripts/Enemy/GameManager.gd
@@ -6,9 +6,7 @@ enum DifficultyOptions {
 	HARD = 2
 }
 
-<<<<<<< HEAD
-var Difficulty : DifficultyOptions = 1
+
 var mouse_sensitivity: float = 1
-=======
+
 var Difficulty : DifficultyOptions = DifficultyOptions.NORMAL
->>>>>>> d87276776e8af6007ae11ab3d6ef7edadb3edfdd
