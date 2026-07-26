95e7fdd06cb143f8d2d6725c355dae01054f60f1
Robert Peters <robip2305@gmail.com>
2026-07-26 18:05:46 +0200

    Multiple Enemies

--- Scripts/Enemy/enemy_spawner.gd
+++ Scripts/Enemy/enemy_spawner.gd
@@ -1,21 +1,26 @@
 extends Node
 
-@export var mob_scene: PackedScene
-@export var player:CharacterBody3D
-# Called when the node enters the scene tree for the first time.
-
+# This creates an expandable list in the Inspector
+@export var mob_scenes: Array[PackedScene] = []
+@export var player: CharacterBody3D
 
 func _on_boss_spawn_enemies() -> void:
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instantiate()
-	# Choose a random location on the SpawnPath.
-	# We store the reference to the SpawnLocation node.
+	# Safety check: make sure the array isn't empty before trying to pick one
+	if mob_scenes.is_empty():
+		push_warning("No mobs assigned in the Inspector!")
+		return
+
+	# Pick a random scene directly from the exported array
+	var chosen_scene = mob_scenes.pick_random()
+	var mob = chosen_scene.instantiate()
+	
+	# Choose a random location on the SpawnPath
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	# And give it a random offset.
 	mob_spawn_location.progress_ratio = randf()
-	#var player_position =player.position
-	#mob.initialize(mob_spawn_location.position, player_position)
+	
+	# Assign the target and position
 	mob.target = player
 	mob.position = mob_spawn_location.position
-	# Spawn the mob by adding it to the Main scene.
+	
+	# Spawn the mob by adding it to the Main scene
 	add_child(mob)
