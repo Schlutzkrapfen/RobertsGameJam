022daad1de16b812ab294cf8e443de16845cd7bd
Robert Peters <robip2305@gmail.com>
2026-07-26 10:26:02 +0200

    Charge Color

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -158,8 +158,10 @@ func _process(delta):
 		ultimateReady = false
 		isChargingUlt = true #NEW CHARGE
 		curUltimateCharge = 0
-	if(curUltimateCharge > 100):
+	if(curUltimateCharge >= 100):
 		ultimateReady = true
+		isChargingUlt = false
+		curUltimateCharge = 100
 	
 	ultChargeUI.value = curUltimateCharge
 	
